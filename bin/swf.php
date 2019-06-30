b5653d693d0de3006eea963123be16936cd06204
aizxin <aizxin@gmail.com>
2019-06-30 08:58:33 +0800

    bin

--- bin/swf.php
+++ bin/swf.php
@@ -1,16 +1,22 @@
 #!/usr/bin/env php
 <?php
 
-require 'vendor/autoload.php';
+define('APP_PATH', realpath(getcwd()));
+
+
+require APP_PATH.'/vendor/autoload.php';
 
 use swf\SwfRun;
 use Yaf\Config\Ini;
 
-define('APP_PATH', dirname(__DIR__));
 //错误信息将写入swoole日志中
-error_reporting(-1);
-ini_set('display_errors', 1);
+ini_set('display_errors', 'on');
+ini_set('display_startup_errors', 'on');
+
+error_reporting(E_ALL);
+date_default_timezone_set('Asia/Shanghai');
 
 $config = (new Ini(APP_PATH . "/conf/application.ini",ini_get('yaf.environ')))->toArray();
 
-(new SwfRun($config))->run(array_shift($argv));
+
+(new SwfRun($config))->run($argv);
